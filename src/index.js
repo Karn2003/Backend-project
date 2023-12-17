49615dec32f33178da18d3264c965fd3ec64daca
karn_keraliya <keraliyakarn@gmail.com>
2023-12-17 15:56:17 +0530

    Write utils for error, asyncronous application call and responce

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
+.then(
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`server is ready to serve at port no ${process.env.PORT}`);
+  })
+)
+.catch((err) => {
+  console.log("MONGO db connection error !!! ", err);
+})
